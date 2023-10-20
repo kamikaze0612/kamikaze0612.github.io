89c1b8fd10a5be431472eb344ebc45611d9e22cb
Buyantogtokh Bekhbayar <buynaa@Buyantogtokhs-MacBook-Pro.local>
2023-10-20 21:55:39 +0900

    Newly created projects added
    Song of Ice and Fire Wiki, Tasker, Globist

--- script.js
+++ script.js
@@ -1,74 +1,74 @@
-const headerEl = document.querySelector('.header');
-const goToUpBtn = document.querySelector('.up-btn');
-const allLinks = document.querySelectorAll('a:link');
-const projectBoxEls = document.querySelectorAll('.project-box');
-const projectPopups = document.querySelectorAll('.popup');
-const popupCloseBtns = document.querySelectorAll('.popup__close-btn');
-const mobileNavBtn = document.querySelector('.btn--mobile-nav');
-const headerNav = document.querySelector('.header__nav');
+const headerEl = document.querySelector(".header");
+const goToUpBtn = document.querySelector(".up-btn");
+const allLinks = document.querySelectorAll("a:link");
+const projectBoxEls = document.querySelectorAll(".project-box");
+const projectPopups = document.querySelectorAll(".popup");
+const popupCloseBtns = document.querySelectorAll(".popup__close-btn");
+const mobileNavBtn = document.querySelector(".btn--mobile-nav");
+const headerNav = document.querySelector(".header__nav");
 
 /////////////////////////////////////////
 // FUNCTIONS
 const openPopup = function (targetEl) {
-  targetEl.classList.add('active');
-  const overlay = targetEl.closest('.popup__overlay');
-  overlay.classList.remove('hidden');
-  document.body.classList.add('popup-show');
+  targetEl.classList.add("active");
+  const overlay = targetEl.closest(".popup__overlay");
+  overlay.classList.remove("hidden");
+  document.body.classList.add("popup-show");
 };
 
 const closePopup = function () {
   const targetEl = Array.from(projectPopups).find((element) =>
-    element.classList.contains('active')
+    element.classList.contains("active")
   );
   projectPopups.forEach((popup) => {
-    popup.classList.remove('active');
+    popup.classList.remove("active");
   });
 
   if (!targetEl) return;
-  const overlay = targetEl.closest('.popup__overlay');
-  overlay.classList.add('hidden');
-  document.body.classList.remove('popup-show');
+  const overlay = targetEl.closest(".popup__overlay");
+  overlay.classList.add("hidden");
+  document.body.classList.remove("popup-show");
 };
 
 /////////////////////////////////////////
 // EVENT LISTENERS
-goToUpBtn.addEventListener('click', () => {
+goToUpBtn.addEventListener("click", () => {
   window.scrollTo({
     top: 0,
-    behavior: 'smooth',
+    behavior: "smooth",
   });
 });
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   if (window.scrollY === 0) {
-    document.body.classList.remove('sticky-nav');
+    document.body.classList.remove("sticky-nav");
   }
   if (window.scrollY > 0) {
-    document.body.classList.add('sticky-nav');
+    document.body.classList.add("sticky-nav");
   }
 });
 
 allLinks.forEach((link) => {
-  link.addEventListener('click', (e) => {
-    const href = link.getAttribute('href');
+  link.addEventListener("click", (e) => {
+    const href = link.getAttribute("href");
 
-    if (href === '#') {
+    if (href === "#") {
       e.preventDefault();
       window.scrollTo({
         top: 0,
-        behavior: 'smooth',
+        behavior: "smooth",
       });
-    } else if (href !== '#' && href.startsWith('#')) {
+    } else if (href !== "#" && href.startsWith("#")) {
       e.preventDefault();
       const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: 'smooth' });
+      sectionEl.scrollIntoView({ behavior: "smooth" });
     }
   });
 });
 
 projectBoxEls.forEach((project) => {
-  project.addEventListener('click', (e) => {
-    const projectName = e.target.closest('div.project-box').dataset.name;
+  project.addEventListener("click", (e) => {
+    const projectName = e.target.closest("div.project-box").dataset.name;
 
     const popup = Array.from(projectPopups).find(
       (element) => element.dataset.project === projectName
@@ -79,14 +79,14 @@ projectBoxEls.forEach((project) => {
 });
 
 popupCloseBtns.forEach((button) =>
-  button.addEventListener('click', closePopup)
+  button.addEventListener("click", closePopup)
 );
 
 projectPopups.forEach((popup) => {
-  popup.closest('.popup__overlay').addEventListener('click', closePopup);
+  popup.closest(".popup__overlay").addEventListener("click", closePopup);
 });
 
-mobileNavBtn.addEventListener('click', () => {
-  headerNav.classList.toggle('nav-open');
-  mobileNavBtn.classList.toggle('clicked');
+mobileNavBtn.addEventListener("click", () => {
+  headerNav.classList.toggle("nav-open");
+  mobileNavBtn.classList.toggle("clicked");
 });
